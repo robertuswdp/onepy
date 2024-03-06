ccbe6f919c4b14d62647f832774a4eed76c854df
robertuswdp <robertuswdp@gmail.com>
2024-03-06 17:59:38 +0700

    sesi 1

---
+++ wanda.py/sesi1/main.py
@@ -0,0 +1,9 @@
+welcome_message = "WELCOME TO THE WANPY"
+
+# print("************************")
+print(f"** {welcome_message} **")
+print("************************")
+
+nama_user = input("masukan nama anda: ")
+
+print(f"haloo {nama_user} ! Coba perhatikan goa dibawah ini")
